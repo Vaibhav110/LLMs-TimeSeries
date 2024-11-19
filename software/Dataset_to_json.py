e5e6bcc2449b9b256d2639e7161d838837539fee
Vaibhav110 <38326308+Vaibhav110@users.noreply.github.com>
2024-11-19 01:22:49 -0800

    Update Dataset_to_json.py
    Code updated to take the average of dates

--- software/Dataset_to_json.py
+++ software/Dataset_to_json.py
@@ -29,6 +29,10 @@ def csv_to_json(csv_file_path, json_file_path):
         count_m = 0
         avg_morning_CO = 0
         avg_night_CO = 0
+        four_dates = []
+        four_dates_mdata = []
+        four_dates_ndata = []
+        four_dates_count = 0
 
         # Iterate through each row in the CSV
         for row in csv_list:
@@ -38,28 +42,24 @@ def csv_to_json(csv_file_path, json_file_path):
             Data_CO = row['CO(GT)'] 
             Data_CO = Data_CO.replace(',', '.')
 
-            print("Date = "+ Data_date + " Time = " + str(Data_time))
+            #print("Date = "+ Data_date + " Time = " + str(Data_time))
 
             # Count the number of daya
             if prev_date != Data_date:
-                if count_m != 0:
-                    avg_morning_CO = avg_morning_CO / count_m
+                four_dates.append(prev_date)
+                four_dates_count = four_dates_count + 1
 
-                    data_item = {
-                        "user": f"Average Morning data of CO on {prev_date} is?",
-                        "assistant": avg_morning_CO
-                    }
-                    data_list.append(data_item)
+                if count_m != 0:
+                    avg_morning_CO = round(avg_morning_CO / count_m , 2)
+                    four_dates_mdata.append(avg_morning_CO)
+                else:
+                    four_dates_mdata.append('NA')
 
                 if count_n != 0:
-                
-                    avg_night_CO = avg_night_CO / count_n
-
-                    data_item = {
-                        "user": f"Average Night data of CO on {prev_date} is?",
-                        "assistant": avg_night_CO
-                    }
-                    data_list.append(data_item)
+                    avg_night_CO = round( avg_night_CO / count_n , 2)
+                    four_dates_ndata.append(avg_night_CO)
+                else:
+                    four_dates_ndata.append('NA')
 
                 no_of_days = no_of_days + 1
                 prev_date = Data_date
@@ -67,6 +67,26 @@ def csv_to_json(csv_file_path, json_file_path):
                 count_m = 0
                 avg_morning_CO = 0
                 avg_night_CO = 0
+                if four_dates_count == 5:
+                    print(four_dates)
+                    print(four_dates_mdata)
+                    print(four_dates_ndata)
+                    data_item = {
+                        "user": f"Average day data of CO on {four_dates[0]} is {four_dates_mdata[0]}, on {four_dates[1]} is {four_dates_mdata[1]}, on {four_dates[2]} is {four_dates_mdata[2]}, on {four_dates[3]} is {four_dates_mdata[3]}. What is average day data the next day? Just tell the answer",
+                        "assistant": four_dates_mdata[4]
+                    }
+                    data_list.append(data_item)
+
+                    data_item = {
+                        "user": f"Average night data of CO on {four_dates[0]} is {four_dates_ndata[0]}, on {four_dates[1]} is {four_dates_ndata[1]}, on {four_dates[2]} is {four_dates_ndata[2]}, on {four_dates[3]} is {four_dates_ndata[3]}. What is average night data the next night? Just tell the answer",
+                        "assistant": four_dates_ndata[4]
+                    }
+                    data_list.append(data_item)
+
+                    four_dates = []
+                    four_dates_mdata = []
+                    four_dates_ndata = []
+                    four_dates_count = 0
 
             # Remove the irrelevant data
             if Data_CO == '-200':
@@ -86,7 +106,7 @@ def csv_to_json(csv_file_path, json_file_path):
         json.dump(data_list, json_file, indent=4)
 
 # Replace these paths with the appropriate file paths
-csv_file_path = r"C:\Users\vaibh\OneDrive\Documents\UCLA_Courses\M202A - Embedded Systems\LLMs_ReadingaMaterial\FineTuning\Dataset\AirQualityUCI.csv"
+csv_file_path = r"C:\Users\vaibh\OneDrive\Documents\UCLA_Courses\M202A - Embedded Systems\LLMs_ReadingaMaterial\FineTuning\Dataset\AirQualityUCI_mod.csv"
 #csv_file_path = r"C:\Users\vaibh\OneDrive\Documents\UCLA_Courses\M202A - Embedded Systems\LLMs_ReadingaMaterial\FineTuning\input.csv"
 json_file_path = r"C:\Users\vaibh\OneDrive\Documents\UCLA_Courses\M202A - Embedded Systems\LLMs_ReadingaMaterial\FineTuning\Output.json"
 
